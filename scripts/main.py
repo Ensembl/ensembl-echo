fc64bb01a0faa74ed7573f192848ac16525a24aa
Simarpreet-Kaur-Bhurji <sbhurji@ebi.ac.uk>
2025-03-03 20:06:04 +0000

    Updates to get relatives from HCP files

--- scripts/main.py
+++ scripts/main.py
@@ -1,5 +1,7 @@
 import argparse
-from closest_rel_within_clusters_take2 import get_closest_rel_within_cluster
+#from closest_rel_within_clusters_take2 import get_closest_rel_within_cluster
+from closest_relatives_birds import get_closest_rel_within_cluster
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -7,7 +9,7 @@ def main():
                     description='What the program does',
                     epilog='Text at the bottom of help')
     parser.add_argument('--num_of_rel', type=int, required=True)
-    parser.add_argument('--ncbi_species_path', type=str, required=True)
+    #parser.add_argument('--ncbi_species_path', type=str, required=True)
     parser.add_argument('--species_path', type=str, required=True)
     parser.add_argument('--cluster_path', type=str, required=True)
     parser.add_argument('--input_pep_files', type=str, required=True)
@@ -21,5 +23,5 @@ def main():
 
     if(args.num_of_rel):
         output_file = "output.txt"                           
-        closest_neighbours = get_closest_rel_within_cluster(args.num_of_rel, args.ncbi_species_path, args.species_path, args.cluster_path, args.input_pep_files, args.output_dir)
+        closest_neighbours = get_closest_rel_within_cluster(args.num_of_rel, args.species_path, args.cluster_path, args.input_pep_files, args.output_dir)
 main()
